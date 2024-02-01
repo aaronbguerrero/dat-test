c9383975172f617141e6dbbec0caddd4c03cb436
Aaron Guerrero <aaronbguerrero@gmail.com>
2024-02-01 09:10:07 -0800

    Fix: set default account would unset for all users

--- app/api/accounts/setDefaultAccount/route.ts
+++ app/api/accounts/setDefaultAccount/route.ts
@@ -1,8 +1,10 @@
 import { ObjectId } from "mongodb"
+import { getServerSession } from "next-auth"
 import { NextRequest, NextResponse } from "next/server"
 import clientPromise from "../../../lib/database"
 
 import type { Account } from "../../../types"
+import { AuthOptions } from "../../auth/[...nextauth]/route"
 
 //Set default financial account
 export async function PATCH(request: NextRequest) {
@@ -10,11 +12,13 @@ export async function PATCH(request: NextRequest) {
     _id: ObjectId,
   } = await request.json()
 
+  const session = await getServerSession(AuthOptions)
+
   const client = await clientPromise
   const db = client.db("userData")
 
   const response = await db.collection('accounts').updateOne(
-    {isDefault: true},
+    { userId: new ObjectId(session?.user?.id), isDefault: true},
     {$set: {isDefault: false}}
   )
   .then(async response => {
