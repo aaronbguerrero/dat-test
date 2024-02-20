39f2e4ef1a350f7f32fec9250cd3f2ff348ce7eb
Aaron Guerrero <aaronbguerrero@gmail.com>
2024-02-19 22:15:19 -0800

    Added income and expense data

--- app/api/months/setMonthData/[slug]/route.ts
+++ app/api/months/setMonthData/[slug]/route.ts
@@ -6,7 +6,7 @@ import { getServerSession } from "next-auth/next"
 import { AuthOptions } from '../../../../lib/authOptions'
 import getTransactions from "../../../../lib/getTransactions"
 
-import type { MonthData } from "../../getMonthData/[slug]/route"
+import type { MonthData } from "../../../../types"
 import type { Transaction } from "../../../../types"
 
 //Set month data
@@ -14,7 +14,7 @@ import type { Transaction } from "../../../../types"
   // Transactions and Starting Amount
 //2. Calculate data:
   //-ending amount
-  
+
 //3. Set data if different that what's stored
 
 export async function GET(request: NextRequest, { params }: { params: { slug: string }}) {
@@ -29,6 +29,7 @@ export async function GET(request: NextRequest, { params }: { params: { slug: st
   const db  = client.db("userData")
 
   if (session?.user) {
+    //Get data
     const monthData = await db.collection<MonthData>("months").findOne({
       $expr: {
         $and: [
@@ -43,25 +44,46 @@ export async function GET(request: NextRequest, { params }: { params: { slug: st
 
     const transactions = await getTransactions(db, session, date)
 
-    //Add each transaction's amount to the endingAmount total
+    //Calculate month data
     const startingAmount = Dinero({ 
       amount: monthData.startingAmount.amount, 
       currency: monthData.startingAmount.currency 
     })
     
+    let income = Dinero({ amount: 0, currency: monthData.startingAmount.currency })
+    let expenses = Dinero({ amount: 0, currency: monthData.startingAmount.currency })
     let endingAmount = startingAmount
     transactions.forEach((transaction: Transaction) => {
+      if (transaction.amount.amount >= 0) income = income.add(Dinero({
+        amount: transaction.amount.amount,
+        currency: transaction.amount.currency
+      }))
+      else expenses = expenses.add(Dinero({
+        amount: transaction.amount.amount,
+        currency: transaction.amount.currency
+      }))
+
       endingAmount = endingAmount.add(Dinero({
         amount: transaction.amount.amount,
         currency: transaction.amount.currency
       }))
     })
 
-    if (endingAmount.getAmount() === monthData.endingAmount?.amount) return NextResponse.json(true)
+    //If there is no change, return true
+    if (
+      income.getAmount() === monthData.totalIncome?.amount &&
+      expenses.getAmount() === monthData.totalExpenses?.amount &&
+      endingAmount.getAmount() === monthData.endingAmount?.amount
+      ) return NextResponse.json(true)
 
+    //Otherwise update the month's data
     const response = await db.collection("months").updateOne(
       { _id: monthData._id },
-      { $set: {endingAmount: { amount: endingAmount.getAmount(), currency: session.user.currencyUsed }}}
+      { $set: {
+        totalIncome: { amount: income.getAmount(), currency: session.user.currencyUsed },
+        totalExpenses: { amount: expenses.getAmount(), currency: session.user.currencyUsed },
+        endingAmount: { amount: endingAmount.getAmount(), currency: session.user.currencyUsed },
+      }}
     )
 
     if (response.modifiedCount === 1) return NextResponse.json(true)
