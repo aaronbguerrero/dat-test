d4994ec2ed569c38bed801ec283e6f031a55bc3c
Aaron Guerrero <aaronbguerrero@gmail.com>
2024-02-19 22:09:58 -0800

    Moved monthData type to central types file

--- app/api/months/getMonthData/[slug]/route.ts
+++ app/api/months/getMonthData/[slug]/route.ts
@@ -2,21 +2,9 @@ import { NextRequest, NextResponse } from "next/server"
 import { getServerSession } from "next-auth/next"
 import clientPromise from "../../../../lib/database"
 import { ObjectId } from "mongodb"
-import Dinero from 'dinero.js'
 import { AuthOptions } from '../../../../lib/authOptions'
-type Dinero = typeof Dinero
 
-export interface MonthData {
-  readonly _id: ObjectId,
-  month: string,
-  startingAmount: { amount: number, currency: Dinero.Currency },
-  userSetStartingAmount?: boolean,
-  endingAmount: { amount: number, currency: Dinero.Currency },
-  //lastMonthEndingAmount: { amount: number, currency: Dinero.Currency }, JUST DO STARTING AMOUNT RIGHT
-  dailyBalance: { amount: number, currency: Dinero.Currency }[],
-  totalExpenses: { amount: number, currency: Dinero.Currency },
-  totalIncome: { amount: number, currency: Dinero.Currency },
-}
+import type { MonthData } from "../../../../types"
 
 //Get month data
 export async function GET(request: NextRequest, { params }: { params: { slug: string }}) {
@@ -30,7 +18,6 @@ export async function GET(request: NextRequest, { params }: { params: { slug: st
   const client = await clientPromise
   const db  = client.db("userData")
 
-  //REmove monthdata here, get data, run calcs, return month
   const monthData = await db.collection<MonthData>("months").findOne({
     $expr: {
       $and: [
@@ -44,8 +31,6 @@ export async function GET(request: NextRequest, { params }: { params: { slug: st
     if (!response) return null
     return response
   })
-
-  // const monthData: MonthData
 
   return NextResponse.json(monthData)
 }
